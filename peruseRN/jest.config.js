cafa0241e623a095d5838bc3b6d17bd1f0811b88
Rahul.Pol <>
2022-06-20 23:08:58 +0530

    setup testing for application

---
+++ peruseRN/jest.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  preset: 'jest-expo',
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
+  ],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{ts,tsx}',
+    '!**/app.config.ts',
+    '!**/coverage/**',
+    '!**/metro.config.ts',
+    '!**/node_modules/**',
+    '!**/webpack.config.ts',
+  ],
+  // Todo: Later ensure test coverage is always greater than 80%
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80,
+    },
+  },
+};
