c63ad88ccb1ba3739827d50a9d428c8b488d4c6f
Rahul.Pol <>
2022-06-21 18:53:24 +0530

    Updated icon image for project, tsconfig path in elslint and moved app config to ts file

--- peruseRN/.eslintrc.js
+++ peruseRN/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     {
       files: ['*.ts', '*.tsx', '*.d.ts'],
       parserOptions: {
-        project: './tsconfig.json',
+        project: 'tsconfig.json',
       },
       rules: {
         '@typescript-eslint/no-unused-vars': 'error',
--- peruseRN/app.config.ts
+++ peruseRN/app.config.ts
@@ -3,6 +3,9 @@ import { ExpoConfig } from '@expo/config-types';
 const config: ExpoConfig = {
   name: 'peruseRN',
   slug: 'peruseRN',
+  version: '0.0.1',
+  orientation: 'portrait',
+  icon: './assets/icon_2.png',
 };
 
 export default config;
